cba72d7b0e4f53f252bc2564d05230db534874da
Kai Longmore <14970686+handle-git@users.noreply.github.com>
2019-07-29 10:45:50 +1000

    added program

---
+++ iir.py
@@ -0,0 +1,2 @@
+from requests_html import HTMLSession
+print("------------------------\n\n    Is it raining?\n\n         " + HTMLSession().get("https://isitraining.in/" + HTMLSession().get("https://mylocation.org/").text.split("\n")[96][12:-6]).text.split("\n ")[69][34:-5] + ".\n\n------------------------")
