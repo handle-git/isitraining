50302afff41b4bd4fa1e98e5a6a42547f6c7ce55
BuildTools <unconfigured@null.spigotmc.org>
2020-01-08 15:00:59 +1100

    Improve readability and PEP 8 compliance

--- iir.py
+++ iir.py
@@ -1,2 +1,12 @@
 from requests_html import HTMLSession
-print("------------------------\n\n    Is it raining?\n\n         " + HTMLSession().get("https://isitraining.in/" + HTMLSession().get("https://mylocation.org/").text.split("\n")[96][12:-6]).text.split("\n ")[69][34:-5] + ".\n\n------------------------")
+
+print(
+    "------------------------\n\n    Is it raining?\n\n         "
+    + HTMLSession()
+    .get(
+        "https://isitraining.in/"
+        + HTMLSession().get("https://mylocation.org/").text.split("\n")[96][12:-6]
+    )
+    .text.split("\n ")[69][34:-5]
+    + ".\n\n------------------------"
+)
